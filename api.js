0c7cd80a1571e59346d9af4d8de1ebbf5d1ceae5
jimmyxuexue <1002661758@qq.com>
2022-10-05 11:32:26 +0800

    :memo: 文档更新至最新

--- api.js
+++ api.js
@@ -6,6 +6,7 @@ const {
 	birthday_templateId,
 	pay_templateId,
 	love_templateId,
+	love_message, // 每日情话集合
 } = require('./config').wechat
 const { weather, city } = require('./config').polymerization
 const { getDay, getLoveDay, getBirthday, getPayDay, base } = require('./fnc')
@@ -32,30 +33,6 @@ async function getWeather() {
 		return res.data.result
 	}
 }
-
-const love_message = [
-	{
-		value: '陪你翻山越岭，追着日落看星星',
-		color: '#8C8C8C',
-	},
-	{
-		value: '天也欢喜，地也欢喜，人也欢喜。欢喜你遇到了我，我遇到了你。',
-		color: '#8C8C8C',
-	},
-	{
-		value: '从你的全世界路过，才发现你就是我的全世界。',
-		color: '#8C8C8C',
-	},
-	{
-		value: '岁月荏苒，青灯孤影月为伴，弱水三千不敌昔日你巧笑嫣然。',
-		color: '#8C8C8C',
-	},
-
-	{
-		value: '想见你，在吞云吐月的疾风后，在日复一日的寻常里。',
-		color: '#8C8C8C',
-	},
-]
 
 // 普通日子模板
 async function getTemplate(ACCESS_TOKEN, weather) {
--- config.js
+++ config.js
@@ -1,24 +1,49 @@
 const config = {
-  wechat: {
-    appID: "", //公众号里面取
+	wechat: {
+		appID: '', //公众号里面取
 
-    AppSecret: "", //公众号里面取
+		AppSecret: '', //公众号里面取
 
-    base_templateId: "", // 元气提醒模板的id
+		base_templateId: '', // 元气提醒模板的id
 
-    birthday_templateId: "", // 生日模板id
+		birthday_templateId: '', // 生日模板id
 
-    pay_templateId: "", // 发薪模板id
+		pay_templateId: '', // 发薪模板id
 
-    love_templateId: "", // 相恋模板id
+		love_templateId: '', // 相恋模板id
 
-    token: "jimmyxuexue", //自定义的token
-  },
+		token: 'jimmyxuexue', //自定义的token
 
-  polymerization: {
-    weather: "", // 第三方天气接口 key
-    city: "福州", // 查询福州的天气
-  },
-};
+		love_message: [
+			// 自定义每日情话
+			{
+				value: '陪你翻山越岭，追着日落看星星',
+				color: '#8C8C8C',
+			},
+			{
+				value: '天也欢喜，地也欢喜，人也欢喜。欢喜你遇到了我，我遇到了你。',
+				color: '#8C8C8C',
+			},
+			{
+				value: '从你的全世界路过，才发现你就是我的全世界。',
+				color: '#8C8C8C',
+			},
+			{
+				value: '岁月荏苒，青灯孤影月为伴，弱水三千不敌昔日你巧笑嫣然。',
+				color: '#8C8C8C',
+			},
 
-module.exports = config;
+			{
+				value: '想见你，在吞云吐月的疾风后，在日复一日的寻常里。',
+				color: '#8C8C8C',
+			},
+		],
+	},
+
+	polymerization: {
+		weather: '', // 第三方天气接口 key
+		city: '福州', // 查询福州的天气
+	},
+}
+
+module.exports = config
